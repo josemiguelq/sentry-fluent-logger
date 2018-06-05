f0b9a54bd85266f61ad306c5e2601f0cff5e36f0
josemiguelq <jose.miguelq95@gmail.com>
2018-06-05 09:45:24 -0300

    change to App facado to acces IoC

--- src/Providers/FluentSentryLoggerProvider.php
+++ src/Providers/FluentSentryLoggerProvider.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class SentryFluentLoggerProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('sentry-fluent', function () {
+		App::bind('sentry-fluent', function () {
             return new SentryFluentLogger\Logger\LogHelper();
         });
         require_once __DIR__ . '../../src/Logger/FluentLogger.php';
